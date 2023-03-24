2de07e89258a311afb0f642937d3ecc0be4e4ccf
Ribus <erick_ribus@hotmail.com>
2023-03-24 09:12:29 -0300

    - Adicionada chamada para CommandService

---
+++ PlatformService/PlatformService/SyncDataServices/Http/ICommandDataService.cs
@@ -0,0 +1,9 @@
+﻿using PlatformService.Dtos;
+
+namespace PlatformService.SyncDataServices.Http
+{
+    public interface ICommandDataService
+    {
+        Task SendPlatformToCommand(PlatformReadDto plat);
+    }
+}
