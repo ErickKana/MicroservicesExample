d596babb3b0a43dd9349a6ef73d828de4d053bf4
ErickKana <erick.kana@gmail.com>
2023-05-16 08:49:48 -0300

    - adicionado event listener no CommandService

--- PlatformService/CommandsService/Program.cs
+++ PlatformService/CommandsService/Program.cs
@@ -1,3 +1,4 @@
+using CommandsService.AsyncDataServices;
 using CommandsService.Data;
 using CommandsService.EventProcessing;
 using Microsoft.EntityFrameworkCore;
@@ -14,6 +15,8 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddSingleton<IEventProcessor, EventProcessor>();
+
+builder.Services.AddHostedService<MessageBusSubscriber>();
 
 var app = builder.Build();
 
