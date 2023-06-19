8fa75369aeb8f7a7716b84d6be8160847c14284d
ErickKana <erick.kana@gmail.com>
2023-06-19 17:50:42 -0300

    Ajustes de pendências

--- PlatformService/CommandsService/EventProcessing/EventProcessor.cs
+++ PlatformService/CommandsService/EventProcessing/EventProcessor.cs
@@ -23,7 +23,7 @@ namespace CommandsService.EventProcessing
             switch (eventType)
             {
                 case EventType.PlatformPublished:
-                    //TODO
+                    AddPlatform(message);
                     break;
 
                 default:
@@ -63,6 +63,7 @@ namespace CommandsService.EventProcessing
                     {
                         repo.CreatePlatform(plat);
                         repo.SaveChanges();
+                        Console.WriteLine($"--> Platform added!");
                     }
                     else
                     {
