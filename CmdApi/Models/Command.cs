33dd690a7a100e00b22206a746cc5aafb2de2082
ErickKana <erick.kana@gmail.com>
2023-09-11 22:13:41 -0300

    dia1

---
+++ CmdApi/Models/Command.cs
@@ -0,0 +1,10 @@
+namespace CmdApi.Models
+{
+    public class Command
+    {
+        public int Id { get; set; }
+        public string HowTo { get; set; } = null!;
+        public string Platform { get; set; } = null!;
+        public string CommandLine { get; set; } = null!;
+    }
+}
